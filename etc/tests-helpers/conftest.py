a991ec8cd7ca9ca3571efa5cb0450efe840c3dd5
Joao Rodrigues <jrodrigues@joaorodrigues.co.uk>
2025-10-07 23:04:19 +0100

    v0.0.1

--- etc/tests-helpers/conftest.py
+++ etc/tests-helpers/conftest.py
@@ -3,7 +3,6 @@ from typing import Any
 import pytest
 from syrupy.extensions.json import JSONSnapshotExtension
 
-
 @pytest.fixture
 def snapshot_json(snapshot: Any) -> Any:
     return snapshot.use_extension(JSONSnapshotExtension)
